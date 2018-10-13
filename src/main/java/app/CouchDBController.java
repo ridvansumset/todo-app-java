325073594231c4e0cace01f298bc3a9f33b729dc
ridvansumset <ridvansumset@gmail.com>
2018-10-13 13:00:21 +0300

    remove target/ and comment out unnecessary imported packages

--- src/main/java/app/CouchDBController.java
+++ src/main/java/app/CouchDBController.java
@@ -1,22 +1,38 @@
  package app;
 
  import org.springframework.web.bind.annotation.RequestMapping;
- import org.springframework.web.bind.annotation.RequestParam;
+ // import org.springframework.web.bind.annotation.RequestParam;
  import org.springframework.web.bind.annotation.RestController;
- import com.couchbase.client.java.*;
+ // import com.couchbase.client.java.*;
  import com.couchbase.client.java.document.*;
  import com.couchbase.client.java.document.json.*;
- import com.fasterxml.jackson.core.*;
+ // import com.fasterxml.jackson.core.*;
  // import com.fasterxml.jackson.annotation.*;
- import com.fasterxml.jackson.databind.*;
- import hello.CouchDB;
- import hello.User;
+ // import com.fasterxml.jackson.databind.*;
+ import app.CouchDB;
+ import app.User;
 
 @RestController
   public class CouchDBController {
 
     @RequestMapping("/couchbase")
-    public User couching(@RequestParam(value="id", defaultValue="1") String id) {
+    public User couching() {
+      CouchDB db = new CouchDB();
+      JsonObject userJsonObject = JsonObject.create();
+
+      JsonObject john = JsonObject.create()
+      .put("name", "john")
+      .put("email", "john@couchbase.com")
+      .put("interests", JsonArray.from("some", "thing"));
+
+      JsonDocument userJsonDocument = db.setUser(john);
+      JsonDocument.from(userJsonDocument, userJsonObject);
+      System.out.println(userJsonDocument);
+
+      return new User("uid'im", "email'im");
+    }
+  }
+    /*public User couching(@RequestParam(value="id", defaultValue="1") String id) {
       CouchDB db = new CouchDB();
       ObjectMapper objectMapper = new ObjectMapper();
       JsonDocument userJsonDocument = db.getUser(id);
@@ -25,22 +41,7 @@
 
       return user;
     }
-  }
-      /*public User couching() {
-        CouchDB db = new CouchDB();
-        JsonObject userJsonObject = JsonObject.create();
-
-        JsonObject john = JsonObject.create()
-        .put("name", "john")
-        .put("email", "john@couchbase.com")
-        .put("interests", JsonArray.from("some", "thing"));
-
-        JsonDocument userJsonDocument = db.setUser(john);
-        JsonDocument.from(userJsonDocument, userJsonObject);
-        System.out.println(userJsonDocument);
-
-        return new User("uid'im", "email'im");
-      }*/
+  }*/
 
       /*public String couching(@RequestParam(value="id", defaultValue="1") String id) {
         JsonObject userJsonObject = JsonObject.create();
